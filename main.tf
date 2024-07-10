b64a0b42bfbaa11cfdc1eb9432a81c3a7c68c1e2
Nadeem62 <106730832+Nadeem62@users.noreply.github.com>
2024-07-10 12:46:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,3 +13,11 @@ resource "aws_subnet" "private_subnet" {
   cidr_block = "10.0.1.0/24"
   vpc_id = "aws_vpc.vpc.id"
 }
+resource "aws_iam_role" "lambda" {
+  name = "DevOps-Candidate-Lambda-Role"
+}
+
+resource "aws_lambda_function" "exam_lambda" {
+  function_name = 
+  role          = aws_iam_role.lambda
+}

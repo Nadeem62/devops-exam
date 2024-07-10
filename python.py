873fd3bb9c00e3301b41fbb4dc570238fc24710b
Nadeem62 <106730832+Nadeem62@users.noreply.github.com>
2024-07-10 12:39:51 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,24 @@
+import requests
+
+def lambda_handler(event, context): 
+  url = 'https://bc1yy8dzsg.execute-api.eu-west-1.amazonaws.com/v1/data'
+  headers = {
+    'Content-type': 'application/json'
+    'X-Siemens-Auth': 'test'
+  }
+
+try: 
+  response= requests.get(url), headers=headers)
+
+  data = response.json()
+
+  return {
+    'statusCode': response.satus_code,
+    'body': data
+  }
+except Exception as e:
+  return {
+    'statusCode' : 200,
+    'body': str(e)
+  }
+  
